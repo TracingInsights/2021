b62886730c529fdaaf51bb3113a9407c238a02de
Tracing Insights <101667461+TracingInsights@users.noreply.github.com>
2024-01-13 09:56:58 +0530

    ..

--- tel.py
+++ tel.py
@@ -24,9 +24,10 @@ events = [#'Bahrain Grand Prix',
     # 'Azerbaijan Grand Prix', 'French Grand Prix', 'Styrian Grand Prix',
     # 'Austrian Grand Prix', 'British Grand Prix', 'Hungarian Grand Prix', 
     # 'Belgian Grand Prix', 'Dutch Grand Prix', 'Italian Grand Prix',
-    # 'Russian Grand Prix', 'Turkish Grand Prix', 'United States Grand Prix',
-    # 'Mexico City Grand Prix', 
-    'São Paulo Grand Prix', 'Qatar Grand Prix', 
+    # 'Russian Grand Prix', 'Turkish Grand Prix', 
+    'United States Grand Prix',
+    'Mexico City Grand Prix', 
+    # 'São Paulo Grand Prix', 'Qatar Grand Prix', 
     # 'Saudi Arabian Grand Prix', 'Abu Dhabi Grand Prix', 
 ]
 
