1cc32f8dfc9bffda19b5fca1550d053d7d9884f3
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2024-03-20 20:52:34 +0530

    Update corners.py

--- corners.py
+++ corners.py
@@ -9,7 +9,11 @@ events = [
     'Bahrain Grand Prix', 
 
   'Emilia Romagna Grand Prix', 
-    'Portuguese Grand Prix', 'Spanish Grand Prix', 'Monaco Grand Prix', 
+    'Portuguese Grand Prix',
+    
+    # 'Spanish Grand Prix', 
+    
+    'Monaco Grand Prix', 
     'Azerbaijan Grand Prix', 'French Grand Prix', 'Styrian Grand Prix',
     'Austrian Grand Prix', 'British Grand Prix', 'Hungarian Grand Prix', 
     'Belgian Grand Prix', 'Dutch Grand Prix', 'Italian Grand Prix',
