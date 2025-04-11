a063a4f0cfdd835cca3ac796aa8de34153a8aa37
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-04-11 21:29:24 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -18,18 +18,29 @@ def events_available(year: int) -> any:
 
 
 events = [
+    'Abu Dhabi Grand Prix', 
+    'Austrian Grand Prix',
+    'Azerbaijan Grand Prix',
     'Bahrain Grand Prix', 
-
-  'Emilia Romagna Grand Prix', 
-    # 'Portuguese Grand Prix', 'Spanish Grand Prix', 'Monaco Grand Prix', 
-    # 'Azerbaijan Grand Prix', 'French Grand Prix', 'Styrian Grand Prix',
-    # 'Austrian Grand Prix', 'British Grand Prix', 'Hungarian Grand Prix', 
-    # 'Belgian Grand Prix', 'Dutch Grand Prix', 'Italian Grand Prix',
-    # 'Russian Grand Prix', 'Turkish Grand Prix', 
-    # 'United States Grand Prix',
-    # 'Mexico City Grand Prix', 
-    # 'São Paulo Grand Prix', 'Qatar Grand Prix', 
-    # 'Saudi Arabian Grand Prix', 'Abu Dhabi Grand Prix', 
+    'Belgian Grand Prix',
+    'British Grand Prix',
+     'Dutch Grand Prix',
+    'Emilia Romagna Grand Prix', 
+     'French Grand Prix',
+    'Hungarian Grand Prix', 
+    'Italian Grand Prix',
+    'Mexico City Grand Prix', 
+    'Monaco Grand Prix',
+    'Portuguese Grand Prix',
+    'Qatar Grand Prix', 
+    'Russian Grand Prix',
+    'Saudi Arabian Grand Prix', 
+    'Spanish Grand Prix',  
+    'Styrian Grand Prix',
+    'São Paulo Grand Prix', 
+    'Turkish Grand Prix', 
+    'United States Grand Prix',
+    
 ]
 
 
