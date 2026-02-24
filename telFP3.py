78b816472c68af29981a91481729893b2c900205
Tracing Insights <tracing.insights@gmail.com>
2026-02-24 18:35:35 +0530

    ..

--- telFP3.py
+++ telFP3.py
@@ -72,11 +72,11 @@ AVAILABLE_SESSIONS = [
 ]
 # Select one or more sessions from AVAILABLE_SESSIONS.
 TARGET_SESSIONS = [
-        "Practice 1",
-        "Qualifying",
-        "Practice 2",
+        # "Practice 1",
+        # "Qualifying",
+        # "Practice 2",
         "Sprint Qualifying",
-        "Race",
+        # "Race",
     ]
 invalid_target_sessions = sorted(set(TARGET_SESSIONS) - set(AVAILABLE_SESSIONS))
 if invalid_target_sessions:
